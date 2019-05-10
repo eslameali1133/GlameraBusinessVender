796a8ce6f376a7c768d3c2a5a9232da48fabaf26
eslam <eslamali1133@hotmail.com>
2019-05-10 23:18:52 +0200

    Initial Commit

---
+++ GlameraBusinessVender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlameraBusinessVender
+//
+//  Created by apple on 5/10/19.
+//  Copyright © 2019 Glamera-Business. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
